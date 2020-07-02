0d17f85feabdc0fb9b1c71d230ca2ce865f27d21
chigau-cloud <nunoreneves@gmail.com>
2020-07-03 00:19:24 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ let terminate;
 let color_offset;
 
   p.setup = function() {
-    p.createCanvas(windowWidth, windowHeight);
+    p.createCanvas(300, 600);
     p.pixelDensity(3.0);
     p.colorMode(p.HSB);
     init();
